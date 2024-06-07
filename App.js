7ab8dbd8cfe674cb03c0d3e85c4d5025fef4f0f5
Logan-Roberto <loganrobertoevangelista@gmail.com>
2024-06-07 20:15:45 -0300

    First commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+
+import RotasAcesso from './pages/RotasApp';
+
+export default function App() {
+  return (
+   <NavigationContainer>
+    <RotasAcesso/>
+   </NavigationContainer>
+  );
+}
